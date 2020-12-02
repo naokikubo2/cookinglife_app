83b8906bc5bd29908b539759f7cd155efdc8957a
naokikubo2 <naoki16871@gmail.com>
2020-12-03 01:34:58 +0900

    指摘事項修正 #2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,16 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  before do
-    @user = build(:user)
-  end
+  let(:user) { build(:user)  }
 
   #validation(nameカラムのみ)
   #正常系
   it "is valid with adequet name, email and password" do
-    user = @user
     expect(user).to be_valid
   end
 
   #異常系
   it 'is invalid without a name' do
-    user = @user
     user.name = ""
     user.valid?
     expect(user.errors.messages[:name]).to include("を入力してください")
@@ -23,13 +19,11 @@ RSpec.describe User, type: :model do
 
   #境界値分析
   it 'is valid with 20文字のname' do
-    user = @user
     user.name  = "a" * 20
     expect(user).to be_valid
   end
 
   it 'is invalid with 21文字のname' do
-    user = @user
     user.name  = "a" * 21
     user.valid?
     expect(user.errors.messages[:name]).to include("は20文字以内で入力してください")

e89490e8f923b9a734af5b8aab703823b194a99d
naokikubo2 <naoki16871@gmail.com>
2020-12-17 01:48:43 +0900

    add validation to food_record model #3

--- app/models/food_record.rb
+++ app/models/food_record.rb
@@ -1,3 +1,10 @@
 class FoodRecord < ApplicationRecord
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :food_name, length: { maximum: 25 }
+  validates :healthy_score, :numericality => { :less_than_or_equal_to => 5, :more_than_or_equal_to => 0 } , allow_nil: true
+  validates :total_score, :numericality => { :less_than_or_equal_to => 5, :more_than_or_equal_to => 0 } , allow_nil: true
+  validates :workload_score, :numericality => { :less_than_or_equal_to => 5, :more_than_or_equal_to => 0} , allow_nil: true
+  validates :memo, length: { maximum: 250 }
 end

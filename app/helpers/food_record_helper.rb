fd2094b95c7bf17073fa6a38383de3bab25ae76a
naokikubo2 <naoki16871@gmail.com>
2020-12-19 17:49:09 +0900

    modify range of healthy_score and workload_score #3

--- app/helpers/food_record_helper.rb
+++ app/helpers/food_record_helper.rb
@@ -4,4 +4,27 @@ module FoodRecordHelper
     "☆" * score unless score == nil
   end
 
+  def cov_ScoreToAdverb(adj_m, adj_p, score)
+    if score == 0
+      "普通"
+    elsif score == 1
+      "少し" + adj_p
+    elsif score == 2
+      "まぁまぁ" + adj_p
+    elsif score == 3
+      adj_p
+    elsif score == 4
+      "とても" + adj_p
+    elsif score == -1
+      "少し" + adj_m
+    elsif score == -2
+      "まぁまぁ" + adj_m
+    elsif score == -3
+      adj_m
+    elsif score == -4
+      "とても" + adj_m
+    end
+
+  end
+
 end
--- app/models/food_record.rb
+++ app/models/food_record.rb
@@ -4,9 +4,9 @@ class FoodRecord < ApplicationRecord
   validates :user_id, presence: true
   validates :image, presence: true
   validates :food_name, length: { maximum: 25 }
-  validates :healthy_score, :numericality => { :less_than_or_equal_to => 5, :more_than_or_equal_to => 0 } , allow_nil: true
-  validates :total_score, :numericality => { :less_than_or_equal_to => 5, :more_than_or_equal_to => 0 } , allow_nil: true
-  validates :workload_score, :numericality => { :less_than_or_equal_to => 5, :more_than_or_equal_to => 0} , allow_nil: true
+  validates :healthy_score, numericality: { greater_than: -5, less_than: 5 }, allow_nil: true
+  validates :total_score, numericality: { greater_than: 0, less_than: 6 }, allow_nil: true
+  validates :workload_score, numericality: { greater_than: -5, less_than: 5 }, allow_nil: true
   validates :memo, length: { maximum: 250 }
 
   mount_uploader :image, ImageUploader

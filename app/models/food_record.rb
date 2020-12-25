b0c999e9d81cf32cfedfe34abc3b56fa556d9cdf
naokikubo2 <naoki16871@gmail.com>
2020-12-25 15:18:57 +0900

    modify food_record validation #18

--- app/models/food_record.rb
+++ app/models/food_record.rb
@@ -10,7 +10,7 @@ class FoodRecord < ApplicationRecord
   validates :total_score, numericality: { greater_than: 0, less_than: 6 }, allow_nil: true
   validates :workload_score, numericality: { greater_than: -5, less_than: 5 }, allow_nil: true
   validates :memo, length: { maximum: 250 }
-  validates :food_timing, inclusion: { in: STATUS_VALUES }, allow_nil: true
+  validates :food_timing, inclusion: { in: STATUS_VALUES }, allow_blank: true
 
   mount_uploader :image, ImageUploader
   acts_as_taggable_on :tags

f42030f423a32f559743dd9cc5fa8780a7627038
naokikubo2 <naoki16871@gmail.com>
2020-12-26 12:12:32 +0900

    指摘事項修正(分岐の修正) #7

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,9 +1,11 @@
 module TagsHelper
   def food_link(food_record)
     if food_record.present?
-      link_to food_record.food_name, food_record_path(food_record)
-    else
-      link_to "料理名未登録", food_record_path(food_record)
+      if food_record.food_name.present?
+        link_to food_record.food_name, food_record_path(food_record)
+      else
+        link_to "料理名未設定", food_record_path(food_record)
+      end
     end
   end
 end

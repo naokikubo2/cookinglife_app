eabfa3a3b5d6cf73cd4ab425b70a48ab10fe23a1
naokikubo2 <naoki16871@gmail.com>
2021-01-04 00:18:12 +0900

    modify relation between food_record and food_share #23

--- app/models/food_record.rb
+++ app/models/food_record.rb
@@ -1,6 +1,6 @@
 class FoodRecord < ApplicationRecord
   belongs_to :user
-  has_many :food_shares
+  has_many :food_shares, dependent: :destroy
 
   STATUS_VALUES = %w[morning lunch dinner snack].freeze
 

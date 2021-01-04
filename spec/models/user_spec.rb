d9184912e1d46e0ccb6cfc9db1b12219864b0bed
naokikubo2 <naoki16871@gmail.com>
2021-01-04 19:38:39 +0900

    add user model test about follow_func #29

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,4 +27,43 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors.messages[:name]).to include("は20文字以内で入力してください")
   end
+
+  #フォロー機能
+  let(:user) { create(:user) }
+  let(:other_user) { create(:user) }
+
+  context "following?" do
+    subject { user.following?(other_user) }
+
+    context 'followed' do
+      before {
+        user.follow(other_user.id)
+      }
+      it "true" do
+        expect(subject).to eq(true)
+      end
+    end
+
+    context 'not followed' do
+      it "false" do
+        expect(subject).to eq(false)
+      end
+    end
+  end
+
+  context "follow/unfollow" do
+    it "anable to switch follow unfollow" do
+      user.follow(other_user.id)
+      expect(user.followings.size).to eq(1)
+      expect(user.followings.first.id).to eq(other_user.id)
+
+      user.unfollow(other_user.id)
+      expect(user.followings.size).to eq(0)
+    end
+
+    it "cannnot follow own" do
+      user.follow(user.id)
+      expect(user.followings.size).to eq(0)
+    end
+  end
 end

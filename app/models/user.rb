ccd2cf7d08a4a6bb3958f1e80863ee409bfe8c07
naokikubo2 <naoki16871@gmail.com>
2021-01-04 21:08:37 +0900

    add dependence in user_model #29

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,21 +8,21 @@ class User < ApplicationRecord
 
   has_many :food_records, dependent: :destroy
   has_many :food_shares, dependent: :destroy
-  has_many :relationships
+  has_many :relationships, dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :user
 
   def follow(other_user_id)
-    self.relationships.find_or_create_by(follow_id: other_user_id) unless self.id == other_user_id.to_i
+    relationships.find_or_create_by(follow_id: other_user_id) unless id == other_user_id.to_i
   end
 
   def unfollow(other_user_id)
-    relationship = self.relationships.find_by(follow_id: other_user_id)
+    relationship = relationships.find_by(follow_id: other_user_id)
     relationship.destroy if relationship
   end
 
   def following?(other_user)
-    self.followings.include?(other_user)
+    followings.include?(other_user)
   end
 end

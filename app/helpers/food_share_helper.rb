3452a1493c99135c9971d0d8b22a8f0c99d01dd3
naokikubo2 <naoki16871@gmail.com>
2021-01-09 15:38:45 +0900

    add お裾分け募集期限で、募集ボタンの有無を分ける機能 #24

--- app/helpers/food_share_helper.rb
+++ app/helpers/food_share_helper.rb
@@ -2,4 +2,8 @@ module FoodShareHelper
   def time_format(time)
     l time, formats: :default if time.present?
   end
+
+  def before_limit_time?(food_share)
+    food_share.limit_time > Time.zone.now
+  end
 end

c3c98b9595329b521b63e9b4dd4056b735f4c7eb
naokikubo2 <naoki16871@gmail.com>
2021-01-16 12:18:24 +0900

    add unique index to favorite model #8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_14_152448) do
+ActiveRecord::Schema.define(version: 2021_01_16_030656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_01_14_152448) do
     t.bigint "food_record_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["food_record_id", "user_id"], name: "index_favorites_on_food_record_id_and_user_id", unique: true
     t.index ["food_record_id"], name: "index_favorites_on_food_record_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end

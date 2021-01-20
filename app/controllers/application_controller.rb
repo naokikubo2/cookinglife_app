75d18e6e489f78ed1e1757bd7be57a7f095492c1
naokikubo2 <naoki16871@gmail.com>
2021-01-20 11:17:06 +0900

    add city table and modify name of column  #25

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :prefecture])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :prefecture])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :location_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :location_id])
   end
 end

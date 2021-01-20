32191bb3622b1e39b1662a7a612b08b4935de778
naokikubo2 <naoki16871@gmail.com>
2021-01-20 11:17:06 +0900

    add city table and modify name of column  #25

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_145623) do
+ActiveRecord::Schema.define(version: 2021_01_19_112153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.integer "location_id"
+    t.float "lon"
+    t.float "lat"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -140,7 +149,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_145623) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "prefecture", null: false
+    t.integer "location_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

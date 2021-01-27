0ae7b5e06fa19587de4500cefbd423150e20222f
naokikubo2 <naoki16871@gmail.com>
2021-01-27 15:42:03 +0900

    modify request test of food_records 画面遷移先変更の対応、create時のparamsにtag_listが追加されたことの対応 #48

--- spec/requests/food_records_request_spec.rb
+++ spec/requests/food_records_request_spec.rb
@@ -166,8 +166,10 @@ RSpec.describe "FoodRecords", type: :request do
   describe "POST /food_records/:id/create" do
     context 'valid params' do
       it "redirect to root page" do
-        post food_records_path, params: { food_record: { user_id: current_user.id, image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image1.png')) } }
-        expect(response).to redirect_to(root_path)
+        post food_records_path, params: { food_record: { user_id: current_user.id, image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image1.png')), tag_list: "tag_test#{timestamp}" } }
+        follow_redirect!
+        expect(response.status).to eq(200)
+        expect(response.body).to include("tag_test#{timestamp}")
       end
     end
 
@@ -183,8 +185,8 @@ RSpec.describe "FoodRecords", type: :request do
       it "render new page" do
         set_error_response
         post food_records_path, params: { food_record: { user_id: current_user.id, image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image1.png')) } }
-        expect(response).to redirect_to(root_url)
         follow_redirect!
+        expect(response.status).to eq(200)
         expect(response.body).to include("天気情報の取得に失敗しましたが、登録に成功しました")
       end
     end

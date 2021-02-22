ef18b880449536367d86fc9b8dd9071d778f8ba4
naokikubo2 <naoki16871@gmail.com>
2021-02-22 20:59:58 +0900

    modify top page design #44

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,3 +1,10 @@
 class HomesController < ApplicationController
-  def top; end
+  def top
+    @food_records = current_user.food_records
+    @food_records_followings = current_user.food_records_followings
+
+    # 今日の料理レコメンドを受けとる
+    recommend = FoodRecord.food_recommend(current_user)
+    @food_recommend = recommend if recommend.present?
+  end
 end

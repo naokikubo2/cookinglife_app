77cc192343123527df70ccf40ce564bc78348eb8
naokikubo2 <naoki16871@gmail.com>
2021-02-23 19:32:23 +0900

    add user_info to top_page #44

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,5 +6,12 @@ class HomesController < ApplicationController
     # 今日の料理レコメンドを受けとる
     recommend = FoodRecord.food_recommend(current_user)
     @food_recommend = recommend if recommend.present?
+
+    # 天気情報を受け取る
+    open_weather = Api::OpenWeatherMap::Request.new(current_user.location_id)
+    response = open_weather.request
+    if response['cod'] == 200
+      @weather = Api::OpenWeatherMap::Request.attributes_for(response)
+    end
   end
 end

7611e9d210a7a0014315d3eee2506f27d86c4fa5
naokikubo2 <naoki16871@gmail.com>
2021-02-28 12:25:04 +0900

    modify top page 非ログインユーザのアクセス対応 #44

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,17 +1,19 @@
 class HomesController < ApplicationController
   def top
-    @food_records = current_user.food_records
-    @food_records_followings = current_user.food_records_followings
+    if user_signed_in?
+      @food_records = current_user.food_records
+      @food_records_followings = current_user.food_records_followings
 
-    # 今日の料理レコメンドを受けとる
-    recommend = FoodRecord.food_recommend(current_user)
-    @food_recommend = recommend if recommend.present?
+      # 今日の料理レコメンドを受けとる
+      recommend = FoodRecord.food_recommend(current_user)
+      @food_recommend = recommend if recommend.present?
 
-    # 天気情報を受け取る
-    open_weather = Api::OpenWeatherMap::Request.new(current_user.location_id)
-    response = open_weather.request
-    if response['cod'] == 200
-      @weather = Api::OpenWeatherMap::Request.attributes_for(response)
+      # 天気情報を受け取る
+      open_weather = Api::OpenWeatherMap::Request.new(current_user.location_id)
+      response = open_weather.request
+      if response['cod'] == 200
+        @weather = Api::OpenWeatherMap::Request.attributes_for(response)
+      end
     end
   end
 end

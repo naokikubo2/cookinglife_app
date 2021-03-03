c5ad1e19e187b1f6efd49a55f6d5ce2b6259b537
naokikubo2 <naoki16871@gmail.com>
2021-03-04 00:58:34 +0900

    modify food_share page #44

--- app/helpers/food_share_helper.rb
+++ app/helpers/food_share_helper.rb
@@ -3,6 +3,10 @@ module FoodShareHelper
     l time, formats: :default if time.present?
   end
 
+  def time_short(time)
+    l time, format: :short if time.present?
+  end
+
   def before_limit_time?(food_share)
     food_share.limit_time > Time.zone.now
   end

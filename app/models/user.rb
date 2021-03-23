ec43bee8e519e683979fe9dbef2d77cf12cf448b
naokikubo2 <naoki16871@gmail.com>
2021-03-23 20:06:27 +0900

    modify user model uniquness validationのmysql対応 #87

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :name, presence: true, uniqueness: { case_sensitive: true }, length: { maximum: 20 }
   validates :location_id, presence: true
   validates :address, presence: true
 

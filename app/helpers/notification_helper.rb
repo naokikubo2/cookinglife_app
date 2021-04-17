ee825be156d01228414e2e63e44565c8d64d6561
naokikubo2 <naoki16871@gmail.com>
2021-04-17 16:12:01 +0900

    add コメント、フォロー、いいね振り分け機能 #96

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -2,4 +2,17 @@ module NotificationHelper
   def unchecked_notifications
     @notifications = current_user.passive_notifications.where(checked: false)
   end
+
+  def notification_form(notification)
+    @visiter = notification.visiter
+    #notification.actionがfollowかlikeかcommentか
+    case notification.action
+      when "follow" then
+        tag.a(notification.visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"があなたをフォロー"
+      when "like" then
+        tag.a(notification.visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:food_record_path(notification.food_record_id), style:"font-weight: bold;")+"にいいね"
+      when "comment" then
+        tag.a(@visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:food_record_path(notification.food_record_id), style:"font-weight: bold;")+"にコメント"
+    end
+  end
 end
